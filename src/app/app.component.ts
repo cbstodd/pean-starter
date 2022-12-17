7e9fd78f5b8662fa82c99434adebf426c78881d4
cbstodd <cbstodd@gmail.com>
2022-12-17 03:12:14 -0500

    Started adding font variables in scss and deciding on correct fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mygiftnetwork-postgresql';
+export class AppComponent implements OnInit {
+  title: string = '';
+  subtitle: string = '';
+
+  dummyCSS = '';
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.title = 'My Gift Network - Version 2.0';
+    this.dummyCSS = `this.dummyCSS =
+  p, td, li, ol, ul, div, span {
+  font-family: $body3-sans-font;
+  font-weight: 500;
+  line-height: 2rem;
+  }`;
+    this.subtitle = 'Second version which is a complete re-write using PostgreSQL versus Google Firestore.';
+  }
 }
