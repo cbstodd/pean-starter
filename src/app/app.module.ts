32ebbff294c99a89839ddfb89357cf965824da82
cbstodd <cbstodd@gmail.com>
2022-12-18 19:49:21 -0500

    Added styling variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NzTableModule } from 'ng-zorro-antd/table';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NzTableModule
+    NzTableModule,
+    NzGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
