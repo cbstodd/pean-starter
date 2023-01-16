636cfc3fa43dd7d5e563e3b42fd2ea8a59bbe206
cbstodd <cbstodd@gmail.com>
2023-01-16 18:10:52 -0500

    Fixed bug with the sidenav logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
-import { AppComponent } from './app.component';
 import { Routes } from "@angular/router";
 import { UserModule } from './user/user.module';
 
 export const appRoutes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'users', loadChildren: () => UserModule },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: '**', redirectTo: 'users', pathMatch: 'full' },
 ];
 
 
